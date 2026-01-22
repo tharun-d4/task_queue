5f379c3b5f2db2cc77e8020be962ca4990e9dab3
Tharun <tharun.damera4@gmail.com>
2026-01-22 20:36:38 +0530

    Add a fn to get the job based on the id in queries

--- shared/src/db/queries.rs
+++ shared/src/db/queries.rs
@@ -1,7 +1,7 @@
-use sqlx::{postgres::PgPool, query_scalar};
+use sqlx::{postgres::PgPool, query_as, query_scalar};
 use uuid::Uuid;
 
-use crate::db::models::NewJob;
+use crate::db::models::{Job, NewJob};
 
 pub async fn insert_job(pool: &PgPool, job: NewJob) -> Result<Uuid, sqlx::Error> {
     let job_id = query_scalar(
@@ -22,4 +22,13 @@ pub async fn insert_job(pool: &PgPool, job: NewJob) -> Result<Uuid, sqlx::Error>
     .await?;
 
     Ok(job_id)
+}
+
+pub async fn get_job_by_id(pool: &PgPool, id: Uuid) -> Result<Job, sqlx::Error> {
+    let job = query_as::<_, Job>("SELECT * FROM jobs WHERE id=$1")
+        .bind(id)
+        .fetch_one(pool)
+        .await?;
+
+    Ok(job)
 }
