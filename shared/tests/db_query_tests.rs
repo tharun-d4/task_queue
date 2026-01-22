3f2e275d4858caff2a87c42d50d3ff6dc2df0150
Tharun <tharun.damera4@gmail.com>
2026-01-22 20:41:11 +0530

    Add get_job_by_id test for testing the query

--- shared/tests/db_query_tests.rs
+++ shared/tests/db_query_tests.rs
@@ -1,5 +1,5 @@
 use chrono::Utc;
-use sqlx::{PgPool, types::JsonValue};
+use sqlx::{types::JsonValue, PgPool};
 
 use shared::db::{
     models::{JobStatus, NewJob},
@@ -8,6 +8,23 @@ use shared::db::{
 
 #[sqlx::test(migrations = "../migrations")]
 async fn test_insert_job_returns_job_id(pool: PgPool) -> Result<(), sqlx::Error> {
+    queries::insert_job(
+        &pool,
+        NewJob {
+            job_type: "new_job".to_string(),
+            payload: JsonValue::String("A new job".to_string()),
+            status: JobStatus::Pending,
+            priority: 1,
+            max_retries: 5,
+            created_at: Utc::now(),
+        },
+    )
+    .await?;
+    Ok(())
+}
+
+#[sqlx::test(migrations = "../migrations")]
+async fn test_get_job_by_id(pool: PgPool) -> Result<(), sqlx::Error> {
     let job_id = queries::insert_job(
         &pool,
         NewJob {
@@ -20,5 +37,8 @@ async fn test_insert_job_returns_job_id(pool: PgPool) -> Result<(), sqlx::Error>
         },
     )
     .await?;
+    let job = queries::get_job_by_id(&pool, job_id).await?;
+
+    assert_eq!(job_id, job.id);
     Ok(())
 }
