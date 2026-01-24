c2ca8c94e8f4e28bf0a92e4637461b472023845a
Tharun <tharun.damera4@gmail.com>
2026-01-24 18:47:54 +0530

    Configure db pool, tracing in worker

--- worker/src/main.rs
+++ worker/src/main.rs
@@ -1,4 +1,23 @@
+use tracing::{info, instrument};
+use uuid::Uuid;
+
+use shared::{config::load_config, db::connection, tracing::init_tracing};
+
+async fn sleep(ms: u64) {
+    tokio::time::sleep(std::time::Duration::from_millis(ms)).await;
+}
+
+#[instrument]
 #[tokio::main]
 async fn main() {
-    println!("Its me - Worker");
+    let _trace_guard = init_tracing("worker");
+    let config = load_config("../config").expect("Config Error");
+    let pool = connection::create_pool(&config.database).await.unwrap();
+
+    let worker_id = Uuid::now_v7();
+    info!("[+] Worker - ID: {:?} has started running...", worker_id);
+
+    loop {
+        sleep(10000).await;
+    }
 }
