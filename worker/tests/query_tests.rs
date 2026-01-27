9e871188a92c451e1c0d70e9ce42b2d589362d50
Tharun <tharun.damera4@gmail.com>
2026-01-27 12:08:25 +0530

    Add a claim_job test in worker

--- worker/tests/query_tests.rs
+++ worker/tests/query_tests.rs
@@ -4,11 +4,20 @@ use uuid::Uuid;
 use worker::db::queries;
 
 #[sqlx::test(migrations = "../migrations")]
-async fn register_worker(pool: PgPool) {
+async fn register_worker_test(pool: PgPool) {
     let worker_id = Uuid::now_v7();
     let pid = std::process::id();
 
     queries::register(&pool, worker_id, pid as i32)
         .await
         .unwrap();
+}
+
+#[sqlx::test(
+    migrations = "../migrations",
+    fixtures(path = "../../test_fixtures", scripts("jobs", "workers"))
+)]
+async fn claim_job_test(pool: PgPool) {
+    let worker_id = Uuid::parse_str("019bfe1d-228e-7938-8678-3798f454c236").unwrap();
+    queries::claim_job(&pool, worker_id).await.unwrap();
 }
