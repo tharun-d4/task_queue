f514a30cfc2c2e30b78a00945e1e52593af90197
Tharun <tharun.damera4@gmail.com>
2026-01-27 10:40:31 +0530

    Insert proper test data in the jobs table for testing

--- test_fixtures/jobs.sql
+++ test_fixtures/jobs.sql
@@ -5,13 +5,27 @@ VALUES
   '019bfadc-28bb-781d-9d22-acf23fe50117',
   'send_email',
   '{
-    "to": "to_email@mail.com",
-    "from": "job_scheduler@mail.com",
-    "subject": "This is a sample test",
-    "body": "Yes this is just a sample api test"
+    "from": "job_scheduler@gmail.com",
+    "to": "user@gmail.com",
+    "subject": "Yay! Welcome to Job scheduler",
+    "body": "This is an automated email sent by a job scheduler and its workers."
   }',
   'pending',
   5,
+  3,
+  NOW() AT TIME ZONE 'UTC'
+),
+(
+  '019bfdd5-cc70-7f37-a02a-1ec5849f25df',
+  'send_email',
+  '{
+    "from": "job_scheduler@gmail.com",
+    "to": "user@gmail.com",
+    "subject": "Please complete your registration process",
+    "body": "Hey user, we would like to complete your registration process immediately to win the reward."
+  }',
+  'pending',
+  1,
   5,
   NOW() AT TIME ZONE 'UTC'
 );
