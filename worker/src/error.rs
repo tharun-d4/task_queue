2214dc63f22b4322fd52f40afc2f73a72fc5b15f
Tharun <tharun.damera4@gmail.com>
2026-03-04 18:16:01 +0530

    Create a new worker error type that use anyhow

--- worker/src/error.rs
+++ worker/src/error.rs
@@ -1,9 +1,49 @@
+use std::fmt;
+
 use lettre::{
     address::AddressError, error::Error as LettreError, transport::smtp::Error as SmtpError,
 };
 use reqwest::Error as ReqwestError;
 use sqlx::Error as SqlxError;
 use thiserror::Error;
+
+#[derive(Debug)]
+pub enum ErrorStatus {
+    Temporary,
+    Permanent,
+}
+
+#[derive(Debug)]
+pub struct WorkerErrorV2 {
+    pub status: ErrorStatus,
+    pub message: String,
+    pub source: Option<anyhow::Error>,
+}
+
+impl fmt::Display for WorkerErrorV2 {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "{self:?}")
+    }
+}
+
+impl core::error::Error for WorkerErrorV2 {}
+
+impl WorkerErrorV2 {
+    pub fn permanent(message: &str, source: impl Into<anyhow::Error>) -> Self {
+        Self {
+            status: ErrorStatus::Permanent,
+            message: message.to_string(),
+            source: Some(source.into()),
+        }
+    }
+    pub fn temporary(message: &str, source: impl Into<anyhow::Error>) -> Self {
+        Self {
+            status: ErrorStatus::Temporary,
+            message: message.to_string(),
+            source: Some(source.into()),
+        }
+    }
+}
 
 #[derive(Debug, Error)]
 pub enum WorkerError {
