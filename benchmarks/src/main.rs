0460e59db44a59f2fa5ccf4e330b13b8bb98b580
Tharun <tharun.damera4@gmail.com>
2026-03-12 21:55:50 +0530

    Add a simple compile, run and kill server using process command in benchmarking to begin with

--- benchmarks/src/main.rs
+++ benchmarks/src/main.rs
@@ -1,3 +1,23 @@
+use std::process::Command;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Begin benchmarking...");
+
+    let build_server = Command::new("cargo")
+        .args(["build", "--bin", "server"])
+        .output()
+        .expect("failed to compile server");
+
+    println!("build_server: {:?}", build_server);
+
+    let server_process = Command::new("./target/debug/server")
+        .spawn()
+        .expect("failed to spawn a server process");
+    println!("server_process pid: {:?}", server_process.id());
+
+    // kill the server process by sending SIGTERM
+    Command::new("kill")
+        .args(["-TERM", &server_process.id().to_string()])
+        .output()
+        .expect("Killing server process");
 }
