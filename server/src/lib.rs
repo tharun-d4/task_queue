13c13ee947c5d4b5f8a85d6e863873c1839f1518
Tharun <tharun.damera4@gmail.com>
2026-03-20 22:13:23 +0530

    Move the init code to lib.rs and add comments to it in the server crate

--- server/src/lib.rs
+++ server/src/lib.rs
@@ -4,3 +4,66 @@ pub mod db;
 pub mod error;
 pub mod handlers;
 pub mod state;
+
+use tracing::{info, instrument};
+
+use shared::{config::load_server_config, db::connection, tracing::init_tracing};
+
+/// A HTTP Server through which jobs are
+/// submitted, scheduled, monitored and managed
+/// in the Job Scheduler project.
+///
+/// Jobs are submited via REST API that are stored in the database
+/// so that the worker processes that run/execute the jobs fetch them from
+/// the database table in which they are inserted.
+///
+///
+/// fn init() is the actual fn that setups the server.
+///
+/// The server loads its configuration from a centralized config.yaml file.
+/// server:
+///     host:
+///     port:
+///     db_pool_size:
+///     ...
+///
+/// It connects to the database and creates a connection pool that is used
+/// by the request handlers to perform database operations.
+///
+/// It runs the axum server at the host and port defined in the config file.
+///
+/// Prior to listening to the requests,
+/// the server spawns two specific tasks: Lease recovery and Cleanup.
+///
+/// Lease Recovery Task: Jobs are leased to workers for a specific period of time
+/// (that too is as per the config file).
+/// If the jobs are executed within this time, then that's all well and good.
+/// Otherwise, the jobs are assumed to be stalled or stuck due to a worker crash
+/// or some other reason, and they are recovered so they can be retried
+/// by another worker if the failure was temporary and not a permanent error.
+///
+/// Cleanup Task: It marks the jobs as permanently failed if they couldn't be marked
+/// as such due to worker panicking while executing the jobs.
+///
+#[instrument]
+pub async fn init() -> Result<(), error::ServerError> {
+    let _trace_guard = init_tracing("server");
+    let config = load_server_config("./config").expect("Config Error");
+
+    let pool = connection::create_pool(config.database, config.server.db_pool_size).await?;
+    connection::run_migrations(&pool).await?;
+
+    background::lease_recovery_task(pool.clone(), config.server.lease_recovery).await;
+    background::cleanup_task(pool.clone(), config.server.cleanup).await;
+
+    let state = state::AppState::new(pool);
+    let app = app::create_router(state);
+
+    let bind = format!("{}:{}", config.server.host, config.server.port);
+    info!("[+] Server running on {bind:?}...");
+
+    let listener = tokio::net::TcpListener::bind(bind).await?;
+    axum::serve(listener, app).await?;
+
+    Ok(())
+}
--- server/src/main.rs
+++ server/src/main.rs
@@ -1,40 +1,4 @@
-use tracing::{info, instrument};
-
-use server::{app, background, error, state};
-use shared::{config::load_server_config, db::connection, tracing::init_tracing};
-
-/// Server entrypoint.
-///
-/// Responsible for:
-/// - Initializing tracing/logging
-/// - Loading configuration
-/// - Establishing a database connection
-/// - Running database migrations
-/// - Spawning background maintenance tasks
-/// - Starting the HTTP server
-///
-/// Background tasks (lease recovery, cleanup) are started before starting the server.
-
-#[instrument]
 #[tokio::main]
-async fn main() -> Result<(), error::ServerError> {
-    let _trace_guard = init_tracing("server");
-    let config = load_server_config("./config").expect("Config Error");
-
-    let pool = connection::create_pool(config.database, config.server.db_pool_size).await?;
-    connection::run_migrations(&pool).await?;
-
-    background::lease_recovery_task(pool.clone(), config.server.lease_recovery).await;
-    background::cleanup_task(pool.clone(), config.server.cleanup).await;
-
-    let state = state::AppState::new(pool);
-    let app = app::create_router(state);
-
-    let bind = format!("{}:{}", config.server.host, config.server.port);
-    info!("[+] Server running on {bind:?}...");
-
-    let listener = tokio::net::TcpListener::bind(bind).await?;
-    axum::serve(listener, app).await?;
-
-    Ok(())
+async fn main() -> Result<(), server::error::ServerError> {
+    server::init().await
 }
